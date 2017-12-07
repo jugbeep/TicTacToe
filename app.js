3a2b54a30f3e66b07222a8386165f06190891073
jugbeep <patrickbotkins@gmail.com>
2017-12-06 17:39:25 -0700

    x's and o's on boardgit add .

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 console.log ("JS Linked");
 
 $(document).ready(function(){
-	console.log('jq ready')
+	console.log('jq ready');
+		var count = 0;
+
+		$('.board').click(function(){
+			count += 1;
+
+			if (count % 2 === 0) {
+				$('.cell').click(function () {
+				$(this).html('<p>X</p>')});
+			} else {
+				$('.cell').click(function () {
+				$(this).html('<p>O</p>');
+			});
+		};
+	});
 });
