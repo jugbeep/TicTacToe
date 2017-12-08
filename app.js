7e0e7294999bce5d632711225ed5abf0e683af91
jugbeep <patrickbotkins@gmail.com>
2017-12-07 19:08:01 -0700

    little bit of styling & i think this is passing now

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ $(document).ready(function(){
 	console.log('jq ready');
 
 //global varibles 
-let unchecked = false;
-let win = false;
+
 let count = 0;
+let cell2 = $('#cell2')
 
-let cell1 = $('#cell1');
-
-$('.cell').on('click', function altXandO() {
+$('.cell').on('click', function() {
 
 count ++; 	
 	//check if cell is free
 	if ($(this).is('.played')) {
-		alert ('This box has been clicked! Try an empty box.')
+		return;
 	}; 
 	//alternate x play and o play
 	if (count % 2 === 0) {
 		$(this).removeClass('.open');
 		$(this).addClass('played');
-		$(this).addClass('X');
-		$(this).html('X');
+		$(this).addClass('playedO');
+		$(this).html('O');
 	};
 	if (count % 2 !== 0) {
 		$(this).removeClass('.open');
 		$(this).addClass('played');
-		$(this).addClass('O');
-		$(this).html('O');
+		$(this).addClass('playedX');
+		$(this).html('X');
 	};
 });
 //check for win
-function CheckForWin() {
- 	if ( cell1.hasClass('O')) { //&& $('#cell2').hasClass('O') && $('cell3').hasClass('O')) {
- 		alert ('O Wins!');
+function checkForWin() {
+ 	if (cell2.hasClass('playedO') == true ) { //&& $('#cell2').hasClass('O') && $('cell3').hasClass('O')) {
+ 		console.log('O Wins!');
  	} 
+
 }	
-CheckForWin();
+checkForWin();
 });
+
 ;
 
 	
