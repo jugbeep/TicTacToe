dab01034cac3c07074ab0ec9e4032ee48293e17c
jugbeep <patrickbotkins@gmail.com>
2017-12-07 17:14:45 -0700

    NO DOUBLE CLICKSgit add .

--- app.js
+++ app.js
@@ -2,54 +2,41 @@ console.log ("JS Linked");
 
 $(document).ready(function(){
 	console.log('jq ready');
-		
-// count the board //
-// let counter = 0;
-// $('board').on('click', '.cell', function(){
-// 	counter +1;
-// });	
-// console.log(counter)
-// });
+
+//global varibles 
+let unchecked = false;
+let win = false;
+let count = 0;
 
 
 
+$('.cell').on('click', function altXandO() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////working but buggy/////
-	var count = 1;
-
-	$('.cell').click(function() {
-		$(this).addClass("clicked");
-	});
-
-	$('.board').click(function(){
-		count += 1;
-
-
-		if (count % 2 === 0) {
-			$('.cell').click(function () {
-			$(this).html('<p id="x">X</p>')});
-		} if (count % 2 !== 0) {
-			$('.cell').click(function () {
-			$(this).html('<p id="o">O</p>')});
-		} if (count <= 9) {
-				return ("count >= 9")};
-		});
+count ++; 	
+	//check if cell is free
+	if ($(this).is('.played')) {
+		alert ('This box has been clicked! Try an empty box.')
+	}; 
+	//alternate x play and o play
+	if (count % 2 === 0) {
+		$(this).removeClass('.open');
+		$(this).addClass('played');
+		$(this).html('<p id="x">X</p>');
+	};
+	if (count % 2 !== 0) {
+		$(this).removeClass('.open');
+		$(this).addClass('played');
+		$(this).html('<p id="o">O</p>');
+	};
+});
+//check for win	
 
 });
+;
+
+	
+//clear the board
+//win alert
+//tie alert
+
 
