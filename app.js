05f2ba0068ff46655b4fa41da1f5b2a12814a28a
jugbeep <patrickbotkins@gmail.com>
2017-12-11 16:58:21 -0700

    win logic set

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ $(document).ready(function(){
 
 let count = 0;
 
-//wins
-let win1 = $('#cell1') && $('#cell2') && $('#cell3');
-let win2 = $('#cell4') && $('#cell5') && $('#cell6');
-let win3 = $('#cell7') && $('#cell8') && $('#cell9');
-let win4 = $('#cell1') && $('#cell4') && $('#cell7');
-let win5 = $('#cell2') && $('#cell5') && $('#cell8');
-let win6 = $('#cell3') && $('#cell6') && $('#cell9');
-let win7 = $('#cell7') && $('#cell5') && $('#cell3');
-let win8 = $('#cell1') && $('#cell5') && $('#cell9');
 
 $('.cell').on('click', function() {
 	count ++; 	
@@ -40,57 +31,110 @@ $('.cell').on('click', function() {
 
 //check for X win
 
-	
-	if ($(win1).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win2).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win3).hasClass('playedX')) {
-		alert('X Wins');	
-	} else if ($(win4).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win5).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win6).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win7).hasClass('playedX')) {
-		alert('X Wins');
-	} else if ($(win8).hasClass('playedX')) {
-		alert('X Wins');
-// check if O wins
-	// } if ($(win1).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win2).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win3).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win4).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win5).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win6).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win7).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// } if ($(win8).hasClass('playedO')) {
-	// 	alert('O Wins');
-	// };
+	if ($('#cell1').hasClass('playedX') && $('#cell2').hasClass('playedX') && $('#cell3').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell4').hasClass('playedX') && $('#cell5').hasClass('playedX') && $('#cell6').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell7').hasClass('playedX') && $('#cell8').hasClass('playedX') && $('#cell9').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell1').hasClass('playedX') && $('#cell4').hasClass('playedX') && $('#cell7').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell2').hasClass('playedX') && $('#cell5').hasClass('playedX') && $('#cell8').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell3').hasClass('playedX') && $('#cell6').hasClass('playedX') && $('#cell9').hasClass('playedX') === true) {
+		alert('X Wins!');
+	} if ($('#cell7').hasClass('playedX') && $('#cell5').hasClass('playedX') && $('#cell3').hasClass('playedX') === true) {
+		alert('X Wins!')
+	} if ($('#cell1').hasClass('playedX') && $('#cell5').hasClass('playedX') && $('#cell9').hasClass('playedX') === true) {
+		alert('X Wins!')
+	} 
+//check for O win
 
+	if ($('#cell1').hasClass('playedO') && $('#cell2').hasClass('playedO') && $('#cell3').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell4').hasClass('playedO') && $('#cell5').hasClass('playedO') && $('#cell6').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell7').hasClass('playedO') && $('#cell8').hasClass('playedO') && $('#cell9').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell1').hasClass('playedO') && $('#cell4').hasClass('playedO') && $('#cell7').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell2').hasClass('playedO') && $('#cell5').hasClass('playedO') && $('#cell8').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell3').hasClass('playedO') && $('#cell6').hasClass('playedO') && $('#cell9').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell7').hasClass('playedO') && $('#cell5').hasClass('playedO') && $('#cell3').hasClass('playedO') === true) {
+		alert('O Wins!');
+	} if ($('#cell1').hasClass('playedO') && $('#cell5').hasClass('playedO') && $('#cell9').hasClass('playedO') === true) {
+		alert('O Wins!');
+	};
 
-
-};
 });
 });
 
 
 
-	
-//clear the board
-// //win alert
-// //tie alert
 
-// 	if ($('.cell').hasClass('playedO')) {
-// 		alert('X wins!')
-// 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+// 	if ($('.playedX') && win1 === true) {
+// 		alert('X Wins');
+// 	} if ($('.playedX') && win2 === true) {
+// 		alert('X Wins');
+// 	} else if ($(win3).hasClass('playedX')) {
+// 		alert('X Wins');	
+// 	} else if ($(win4).hasClass('playedX')) {
+// 		alert('X Wins');
+// 	} else if ($(win5).hasClass('playedX')) {
+// 		alert('X Wins');
+// 	} else if ($(win6).hasClass('playedX')) {
+// 		alert('X Wins');
+// 	} else if ($(win7).hasClass('playedX')) {
+// 		alert('X Wins');
+// 	} else if ($(win8).hasClass('playedX')) {
+// 		alert('X Wins');
+// // check if O wins
+// 	} if ($(win1).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win2).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win3).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win4).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win5).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win6).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win7).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	} if ($(win8).hasClass('playedO')) {
+// 		alert('O Wins');
+// 	};
+
+
+
+// });
+// });
+
+
+
+
+
 	
 
